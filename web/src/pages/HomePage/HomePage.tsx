0c952ef04ed808fb84561f58b5a03a75c5526d95
Neil Ryan L <neilryanlipaod@gmail.com>
2022-06-13 22:45:39 +0800

    feat(pages): add homepage

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,15 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+
+      <h1>Redwood Blog</h1>
+
+      <p>Welcome to the BLOG!</p>
+    </>
+  )
+}
+
+export default HomePage
