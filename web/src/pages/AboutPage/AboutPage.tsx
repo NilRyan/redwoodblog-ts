ff02316c43ee5cecb9b88c3f6306c2ddc11d987a
Neil Ryan L <neilryanlipaod@gmail.com>
2022-06-14 20:42:04 +0800

    feat(layouts): add blog layout

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -1,21 +1,10 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const AboutPage = () => {
   return (
     <>
       <MetaTags title="About" description="About page" />
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.home()}>Home</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>Just trying out Redwood!</main>
+      <p>Just trying out redwood</p>
     </>
   )
 }
