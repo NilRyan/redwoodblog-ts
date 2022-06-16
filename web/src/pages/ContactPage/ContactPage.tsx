53e6fc8f63c1830af25a002b1247ce09ee883373
Neil Ryan L <neilryanlipaod@gmail.com>
2022-06-16 21:25:32 +0800

    feat(forms): add contact us form validation

--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -1,5 +1,7 @@
 import {
+  FieldError,
   Form,
+  Label,
   Submit,
   SubmitHandler,
   TextAreaField,
@@ -19,15 +21,43 @@ const ContactPage = () => {
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
-      <Form onSubmit={onSubmit}>
-        <label htmlFor="name">Name</label>
-        <TextField name="name" />
+      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
+        <Label name="name" errorClassName="error">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
 
-        <label htmlFor="email">Email</label>
-        <TextField name="email" />
+        <Label name="email" errorClassName="error">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+            pattern: {
+              value: /^[^@]+@[^.]+\..+$/,
+              message: 'Please enter a valid email address',
+            },
+          }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
 
-        <label htmlFor="message">Message</label>
-        <TextAreaField name="message" />
+        <Label name="message" errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
+
         <Submit>Save</Submit>
       </Form>
     </>
