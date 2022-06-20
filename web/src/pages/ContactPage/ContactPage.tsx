0858ba093a3d8551bb2e50fd75f0e81628de8af8
Neil Ryan Lipa-od <neilryanlipaod@gmail.com>
2022-06-20 15:36:32 +0800

    feat(forms): add backend error handling for contacts

--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -1,13 +1,16 @@
 import {
   FieldError,
   Form,
+  FormError,
   Label,
   Submit,
   SubmitHandler,
   TextAreaField,
   TextField,
+  useForm,
 } from '@redwoodjs/forms'
 import { MetaTags, useMutation } from '@redwoodjs/web'
+import { toast, Toaster } from '@redwoodjs/web/toast'
 import {
   CreateContactMutation,
   CreateContactMutationVariables,
@@ -25,17 +28,32 @@ interface FormValues {
   message: string
 }
 const ContactPage = () => {
-  const [create] = useMutation<
+  const formMethods = useForm()
+  const [create, { loading, error }] = useMutation<
     CreateContactMutation,
     CreateContactMutationVariables
-  >(CREATE_CONTACT)
+  >(CREATE_CONTACT, {
+    onCompleted: () => {
+      toast.success('Thank you for your submission')
+      formMethods.reset()
+    },
+  })
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     create({ variables: { input: data } })
   }
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
-      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
+
+      <Toaster />
+      <Form
+        onSubmit={onSubmit}
+        config={{ mode: 'onBlur' }}
+        error={error}
+        formMethods={formMethods}
+      >
+        <FormError error={error} wrapperClassName="form-error" />
+
         <Label name="name" errorClassName="error">
           Name
         </Label>
@@ -72,7 +90,7 @@ const ContactPage = () => {
         />
         <FieldError name="message" className="error" />
 
-        <Submit>Save</Submit>
+        <Submit disabled={loading}>Save</Submit>
       </Form>
     </>
   )
